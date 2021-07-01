0fa3f1bb793c545014515bd3b86f13ca7364da22
saminsakur8@gmail.com <saminsakur8@gmail.com>
2021-07-01 22:33:10 +0600

    bot replies to mentions with whatever user writes after @mention

--- main.py
+++ main.py
@@ -38,22 +38,17 @@ def matcheachwrd(content, list_to_match):
     y = any([i.capitalize() in content for i in list_to_match])
     return x or y
 
-def matchstartswith(message, content, list_to_match:list):
-    if not client.user.mentioned_in(message):
-        n1 = []
-        n2 = []
-        for i in list_to_match:
-            c1 = content.startswith(i)              # Cheches if the content is present in the list
-            n1.append(c1)
+def matchstartswith(content, list_to_match:list):
+    n1 = []
+    n2 = []
+    for i in list_to_match:
+        c1 = content.startswith(i)              # Cheches if the content is present in the list
+        n1.append(c1)
 
-            c2 = content.startswith(i.capitalize()) # Capitalizes the item of the list and tries checkes again
-            n2.append(c2)
+        c2 = content.startswith(i.capitalize()) # Capitalizes the item of the list and tries checkes again
+        n2.append(c2)
         
-        return any(n1) or any(n2)   # If one of them True, returns True else returns False
-    else:
-        x = message.content.replace(client.user.mention)
-        matchstartswith(x, "")
-        print("Hello world!")
+    return any(n1) or any(n2)   # If one of them True, returns True else returns False
 
 class BotClient(discord.Client):
     async def on_ready(self):
@@ -76,22 +71,22 @@ class BotClient(discord.Client):
         if matchwhole(msg, greetings_words):
             await message.reply(random.choice(greetings_words))
 
-        elif matchstartswith(message, msg, special_grettings):
+        elif matchstartswith(msg, special_grettings):
             await message.reply(random.choice(greetings_words))
 
-        elif matchstartswith(message, msg, hello_to_bot):
+        elif matchstartswith(msg, hello_to_bot):
             await message.reply(random.choice(greetings_words))
 
-        elif matchstartswith(message, msg, health_question):
+        elif matchstartswith(msg, health_question):
             await message.reply(random.choice(health_question_response))
 
-        elif matchstartswith(message, msg, health_question_response):
+        elif matchstartswith(msg, health_question_response):
             await message.reply(random.choice(health_question_response_reply))
 
-        elif matchstartswith(message, msg, bot_question):
+        elif matchstartswith(msg, bot_question):
             await message.reply("Yes I am!")
 
-        elif matchstartswith(message, msg, question_self_health):
+        elif matchstartswith(msg, question_self_health):
             await message.reply("Oh! How are you? 😃")
 
         elif matchwhole(msg, too_like_pharse):
@@ -100,52 +95,59 @@ class BotClient(discord.Client):
         elif msg == "ping":
             await message.reply("pong", mention_author=True)
 
-        elif matchstartswith(message, msg, some_random_words):
+        elif matchstartswith(msg, some_random_words):
             await message.reply("Hey! 😀", mention_author=True)
 
-        elif matchstartswith(message, msg, ["Is Mahin a dumb?", "is mahin a dumb?"]):
+        elif matchstartswith(msg, ["Is Mahin a dumb?", "is mahin a dumb?"]):
             await message.reply("Yes he is 🤣")
 
-        elif matchstartswith(message, msg, ["Is Samin a dumb?", "is samin a dumb?"]):    # ME
+        elif matchstartswith(msg, ["Is Samin a dumb?", "is samin a dumb?"]):    # ME
             await message.reply("Aren't you a human? A human can tell this better.")
 
-        elif matchstartswith(message, msg, making_sad_words):
+        elif matchstartswith(msg, making_sad_words):
             await message.reply("Ouch! 🥺")
 
         elif matcheachwrd(msg, shutting_bot):
             await message.reply("Okay 🤐")
     
-        elif matchstartswith(message, msg, thanking_words):
+        elif matchstartswith(msg, thanking_words):
             await message.reply("Welcome!")
 
-        elif matchstartswith(message, msg, fun_reaction):
+        elif matchstartswith(msg, fun_reaction):
             await message.reply(random.choice(fun_reaction_reply))
 
         elif message.content == "I'm from the east side of america":
             await message.reply(
                 "Where we choose pride over character\nAnd we can pick sides, but this is us, this is us, this is")
 
-        elif matchstartswith(message, msg, ["Who made you?", "who made you?", "Where are you from?"]):
+        elif matchstartswith(msg, ["Who made you?", "who made you?", "Where are you from?"]):
             await message.channel.send("I'm a bot made by @sam.in#3588")
 
-        elif matchstartswith(message, msg, askingforjoke):
+        elif matchstartswith(msg, askingforjoke):
             jsonJoke = getJoke(jokesUrl)
             jokeType = jsonJoke['type']
             setup = jsonJoke['setup']
             punchline = jsonJoke['punchline']
             await message.channel.send(f"**{jokeType}**\n{setup}\n{punchline}")
 
-        elif matchstartswith(message, msg, telling_for_quotes):
+        elif matchstartswith(msg, telling_for_quotes):
             await message.channel.send(getQuote(quotesUrl))
 
-        elif matchstartswith(message, msg, nice_words):
+        elif matchstartswith(msg, nice_words):
             await message.reply(random.choice(health_question_response_reply))
 
         elif matcheachwrd(msg, ["Good"]):
             await message.reply(random.choice(health_question_response_reply))
 
-        # if client.user.mentioned_in(message):
-        #     await message.channel.send('You mentioned me!')
+        if client.user.mentioned_in(message):
+            mentionspace = mention+" "
+            if mention in message.content:
+                x = message.content.replace(mention, "")
+
+            if mentionspace in message.content:
+                x = message.content.replace(mention+" ", "")
+
+            await message.reply(x)
 
 
         # else:                                         # Uncomment if you like    
