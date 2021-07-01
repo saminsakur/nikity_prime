f6c4b457f4ebd0acc4190e5f95b1825d5888bf36
saminsakur8@gmail.com <saminsakur8@gmail.com>
2021-07-01 22:55:20 +0600

    Bot now responds to mentions

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ from decouple import config
 jokesUrl = r"https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/random/jokes"
 quotesUrl = r"https://zenquotes.io/api/random"
 
+mention = f'<@!858740782503952384>'
+
 
 def getJoke(url):
     r = requests.get(url)
@@ -38,17 +40,52 @@ def matcheachwrd(content, list_to_match):
     y = any([i.capitalize() in content for i in list_to_match])
     return x or y
 
-def matchstartswith(content, list_to_match:list):
-    n1 = []
-    n2 = []
-    for i in list_to_match:
-        c1 = content.startswith(i)              # Cheches if the content is present in the list
-        n1.append(c1)
+def matchstartswith(message, content, list_to_match:list):
+    mentionspace = mention+" "
+    
+    if client.user.mentioned_in(message):
+        # if mention in message.content:
+        #     x = message.content.replace(mention, "")
+        #     print(x)
+        #     n1 = []
+        #     n2 = []
+        #     for i in list_to_match:
+        #         c1 = x.startswith(i)              # Cheches if the content is present in the list
+        #         n1.append(c1)
 
-        c2 = content.startswith(i.capitalize()) # Capitalizes the item of the list and tries checkes again
-        n2.append(c2)
+        #         c2 = x.startswith(i.capitalize()) # Capitalizes the item of the list and tries checkes again
+        #         n2.append(c2)
         
-    return any(n1) or any(n2)   # If one of them True, returns True else returns False
+        #     return any(n1) or any(n2)   # If one of them True, returns True else returns False
+
+
+        if mentionspace in message.content:
+            y = message.content.replace(mentionspace, "")
+            print(y)
+            n1 = []
+            n2 = []
+            for i in list_to_match:
+                c1 = y.startswith(i)              # Cheches if the content is present in the list
+                n1.append(c1)
+
+                c2 = y.startswith(i.capitalize()) # Capitalizes the item of the list and tries checkes again
+                n2.append(c2)
+                
+            return any(n1) or any(n2)   # If one of them True, returns True else returns False
+
+
+    else:
+        n1 = []
+        n2 = []
+        for i in list_to_match:
+            c1 = content.startswith(i)              # Cheches if the content is present in the list
+            n1.append(c1)
+
+            c2 = content.startswith(i.capitalize()) # Capitalizes the item of the list and tries checkes again
+            n2.append(c2)
+        
+        return any(n1) or any(n2)   # If one of them True, returns True else returns False
+    
 
 class BotClient(discord.Client):
     async def on_ready(self):
@@ -58,7 +95,6 @@ class BotClient(discord.Client):
 
     async def on_message(self, message):
         msg = message.content
-        mention = f'<@!{self.user.id}>'
 
         if not message.author.id == self.user.id:
             print(f"[INFO] A messege recived from {message.author.name} {message.author.id} - {msg}")
@@ -71,22 +107,22 @@ class BotClient(discord.Client):
         if matchwhole(msg, greetings_words):
             await message.reply(random.choice(greetings_words))
 
-        elif matchstartswith(msg, special_grettings):
+        elif matchstartswith(message, msg, special_grettings):
             await message.reply(random.choice(greetings_words))
 
-        elif matchstartswith(msg, hello_to_bot):
+        elif matchstartswith(message, msg, hello_to_bot):
             await message.reply(random.choice(greetings_words))
 
-        elif matchstartswith(msg, health_question):
+        elif matchstartswith(message, msg, health_question):
             await message.reply(random.choice(health_question_response))
 
-        elif matchstartswith(msg, health_question_response):
+        elif matchstartswith(message, msg, health_question_response):
             await message.reply(random.choice(health_question_response_reply))
 
-        elif matchstartswith(msg, bot_question):
+        elif matchstartswith(message, msg, bot_question):
             await message.reply("Yes I am!")
 
-        elif matchstartswith(msg, question_self_health):
+        elif matchstartswith(message, msg, question_self_health):
             await message.reply("Oh! How are you? 😃")
 
         elif matchwhole(msg, too_like_pharse):
@@ -95,59 +131,52 @@ class BotClient(discord.Client):
         elif msg == "ping":
             await message.reply("pong", mention_author=True)
 
-        elif matchstartswith(msg, some_random_words):
+        elif matchstartswith(message, msg, some_random_words):
             await message.reply("Hey! 😀", mention_author=True)
 
-        elif matchstartswith(msg, ["Is Mahin a dumb?", "is mahin a dumb?"]):
+        elif matchstartswith(message, msg, ["Is Mahin a dumb?", "is mahin a dumb?"]):
             await message.reply("Yes he is 🤣")
 
-        elif matchstartswith(msg, ["Is Samin a dumb?", "is samin a dumb?"]):    # ME
+        elif matchstartswith(message, msg, ["Is Samin a dumb?", "is samin a dumb?"]):    # ME
             await message.reply("Aren't you a human? A human can tell this better.")
 
-        elif matchstartswith(msg, making_sad_words):
+        elif matchstartswith(message, msg, making_sad_words):
             await message.reply("Ouch! 🥺")
 
         elif matcheachwrd(msg, shutting_bot):
             await message.reply("Okay 🤐")
     
-        elif matchstartswith(msg, thanking_words):
+        elif matchstartswith(message, msg, thanking_words):
             await message.reply("Welcome!")
 
-        elif matchstartswith(msg, fun_reaction):
+        elif matchstartswith(message, msg, fun_reaction):
             await message.reply(random.choice(fun_reaction_reply))
 
         elif message.content == "I'm from the east side of america":
             await message.reply(
                 "Where we choose pride over character\nAnd we can pick sides, but this is us, this is us, this is")
 
-        elif matchstartswith(msg, ["Who made you?", "who made you?", "Where are you from?"]):
+        elif matchstartswith(message, msg, ["Who made you?", "who made you?", "Where are you from?"]):
             await message.channel.send("I'm a bot made by @sam.in#3588")
 
-        elif matchstartswith(msg, askingforjoke):
+        elif matchstartswith(message, msg, askingforjoke):
             jsonJoke = getJoke(jokesUrl)
             jokeType = jsonJoke['type']
             setup = jsonJoke['setup']
             punchline = jsonJoke['punchline']
             await message.channel.send(f"**{jokeType}**\n{setup}\n{punchline}")
 
-        elif matchstartswith(msg, telling_for_quotes):
+        elif matchstartswith(message, msg, telling_for_quotes):
             await message.channel.send(getQuote(quotesUrl))
 
-        elif matchstartswith(msg, nice_words):
+        elif matchstartswith(message, msg, nice_words):
             await message.reply(random.choice(health_question_response_reply))
 
         elif matcheachwrd(msg, ["Good"]):
             await message.reply(random.choice(health_question_response_reply))
 
-        if client.user.mentioned_in(message):
-            mentionspace = mention+" "
-            if mention in message.content:
-                x = message.content.replace(mention, "")
-
-            if mentionspace in message.content:
-                x = message.content.replace(mention+" ", "")
-
-            await message.reply(x)
+        # if client.user.mentioned_in(message):
+        #     await message.channel.send('You mentioned me!')
 
 
         # else:                                         # Uncomment if you like    
